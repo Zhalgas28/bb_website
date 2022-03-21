9688a1d5d441d5cad3ff8821a4e37da19836e44e
zhalgas <kabdulov1728@gmail.com>
2022-03-21 23:47:02 +0600

    adding sending a message after the release of the movie

--- blockbuster/contact/service.py
+++ blockbuster/contact/service.py
@@ -1,4 +1,9 @@
 from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from app.models import Movie
+from contact.models import Contact
 
 
 def send(user_email):
@@ -9,3 +14,21 @@ def send(user_email):
         [user_email],
         fail_silently=False,
     )
+
+
+@receiver(post_save, sender=Movie)
+def send_movie(instance, **kwargs):
+    name = instance.name
+    genre = instance.genre
+    year = instance.year
+    description = instance.description
+
+    for user_email in Contact.objects.all():
+        send_mail(
+            f'Обновление на сайте: {name} ({year})',
+            f'{description}',
+            'blackhorse11zhalgas@gmail.com',
+            [user_email],
+            fail_silently=False,
+        )
+
